863a39506e9f6b18fed7f036a58bac7615e2a1e6
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2022-12-22 21:40:26 +0100

    updated js function for setting active menu link

--- assets/js/helpers/activeMenuLink.js
+++ assets/js/helpers/activeMenuLink.js
@@ -1,3 +1,15 @@
-module.exports = function (active) {
-    return (typeof active !== undefined && active === true) ? 'active' : '';
+module.exports = function (url, menuType) {
+    let activeURL;
+
+    switch (menuType) {
+        case "mobilemenu":
+            activeURL = document.querySelector('[data-htsa-id="mobileMenu"]').getAttribute('data-htsa-active-link');
+            break;
+        case "pcmenu":
+            activeURL = document.querySelector('[data-htsa-id="pcMenuRight"]').getAttribute('data-htsa-active-link');
+            break;
+        default:
+            break;
+    }
+    return (typeof activeURL !== undefined && activeURL === url) ? 'active' : '';
 }
